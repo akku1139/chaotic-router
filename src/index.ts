d8469660ebd4cb30ce00cd2a98372a2f5aa7499d
Akari Tsuyukusa <akkun11.open@gmail.com>
2026-05-04 01:43:35 +0900

    export StatefulProvider
    Signed-off-by: Akari Tsuyukusa <akkun11.open@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@
 
 export { ChaoticRouter } from './router.ts';
 export type { ProviderConfig, ChaoticRouterOptions } from './router.ts';
-export { BaseProvider, UpstreamProvider } from './provider.ts';
+export { BaseProvider, UpstreamProvider, StatefulProvider } from './provider.ts';
 export type { Chunk, OpenAIRequest, OpenAIResponse, OpenAIStreamChunk } from './types.ts';
